8170fbc90c48d4ddb79b88558df336db46381bf9
Bradley Aboua <clubrocker13@gmail.com>
2023-07-24 13:00:07 -0400

    trying something out

--- main.js
+++ main.js
@@ -2,15 +2,18 @@
 import cards from "./riderwaite.js"; 
 import Tarotdeck from "./Tarotdeck.js"; 
 
-const deck = new Tarotdeck("Rider-Waite", cards); 
-const x = deck.imagesofcards[78];   
+const deck = new Tarotdeck("Rider-Waite", cards); // passes in the name of the deck and the cards array which is strings
+
+
+
+const x = deck.imagesofcards;   
 const y = deck.backofcards; 
 const content = `
 <h1> TAROT WEB APPLICATION</h1>
 <h2> Welcome to your very own tarot reader online!</h2>
 <h3> Press the button below to get started with using the online tarot reader</h3>
 <div class ="card_box">
-<img src="${y}" alt="back">
+<img src="${x[57]}" alt="back">
 </div>
 <button class="btn">Start</button>`; 
 
