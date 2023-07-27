427dcfc3715b74389d917f9d74bec9f8111ed7b8
Bradley Aboua <clubrocker13@gmail.com>
2023-07-27 19:27:37 -0400

    fixed dealCards function working on printing cards

--- Tarotdeck.js
+++ Tarotdeck.js
@@ -11,14 +11,14 @@ class Tarotdeck {
     }
     shuffleCards () //method that randomizes the cards
     {  
-        let x, y, z; 
-        x = this.imagesofcards; 
-        for (let i = x.length -1; i >0; i--) {
+        let y, x, i, z;
+        x = this.imagesofcards;  
+        for (i = (x.length -1); i > 0; i--) {
             y = Math.floor(Math.random() * (i + 1)); 
             z = x[i]; 
             x[i] = x[y]; 
-            x[y] = z; 
-            this.imagesofcards[i] = z; 
+            x[y] = z;
+
         }
 
         return this.imagesofcards; // returns the shuffled up array of cards 
@@ -27,32 +27,20 @@ class Tarotdeck {
     
     dealcards(type) //passes in an array of strings that are the cards
     {
-       // a = this.shuffleCards()
-        let b = [];
-        // let first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth;
-        // b.add(first);
-        // b.add(second); 
-        // b.add(third); 
-        // b.add(fourth); 
-        // b.add(fifth); 
-        // b.add(sixth);
-        // b.add(seventh); 
-        // b.add(eighth); 
-        // b.add(ninth); 
-        // b.add(tenth); 
+        let b = []; 
     if (type == "celtic" )
     {
         for (let i = 0; i <10; i++)
         {
             let n = this.imagesofcards[i]; 
-            b.push(n); 
+            b[i] = n; 
             if ((n == this.backofcards))
             {
-                b.pop(this.imagesofcards[i]);
-                b.push(this.imagesofcards[i+50]); 
+                b.pop();
+                b[i]=this.imagesofcards[i+50]; 
             }
         }
-        return b; 
+        b; //is the array of the cards
     }; 
 
     if (type == "3")
@@ -60,11 +48,11 @@ class Tarotdeck {
         for (let i = 0; i <3; i++)
         {
             let n = this.imagesofcards[i]; 
-            b.push(n); 
+            b[i]= n; 
             if ((n == this.backofcards))
             {
-                b.pop(this.imagesofcards[i]);
-                b.push(this.imagesofcards[i+50]); 
+                b.pop();
+                b[i]= this.imagesofcards[i+50]; 
             }
         }
         return b; 
@@ -74,15 +62,20 @@ class Tarotdeck {
         for (let i = 0; i <1; i++)
         {
             let n = this.imagesofcards[i]; 
-            b.push(n); 
+            b[i] = n; 
             if ((n == this.backofcards))
             {
-                b.pop(this.imagesofcards[i]);
-                b.push(this.imagesofcards[i+50]); 
+                b.pop();
+                b[i] = this.imagesofcards[i+50]; 
             }
         }
         return b; 
     };
     }
+
+  printCards()
+  {
+    
+  }
 }; 
     export default Tarotdeck;
